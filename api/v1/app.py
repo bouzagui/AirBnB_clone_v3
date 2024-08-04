31329405c77cb479ba949dcb246a8dd48e7b1ce4
bouzagui <badrbouzagui@gmail.com>
2024-08-04 22:57:27 +0100

    done ✅

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""models doc"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*", {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close(error):
+    """close storage"""
+    storage.close()
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from api.v1.views import app_views
 
+
 @app_views.route("/status", strict_slashes="False", methods=["GET"])
 def status():
     return {
