3d292490e718df9e09641cc8ab5ffe5ef8a13085
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-05 02:18:55 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""models doc"""
-from flask import Flask
+"""Index route"""
+from flask import jsonify
 from api.v1.views import app_views
 
-
-@app_views.route("/status", strict_slashes="False", methods=["GET"])
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
-    return {
-        "status": "OK"
-    }
+    return jsonify({"status": "OK"})
