acb2c371e8098dbb3571e7363e46eb583646f975
bouzagui <badrbouzagui@gmail.com>
2024-08-05 01:24:13 +0100

    done ✅

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
@@ -14,6 +15,16 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def close(error):
     """close storage"""
     storage.close()
+
+if os.getenv("HBNB_API_HOST"):
+    host = os.getenv("HBNB_API_HOST")
+else:
+    host = "0.0.0.0"
+
+if os.getenv("HBNB_API_PORT"):
+    port = os.getenv("HBNB_API_PORT")
+else:
+    port = 5000
 
 
 if __name__ == "__main__":
